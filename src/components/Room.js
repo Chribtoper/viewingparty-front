2440910fdf482ddd7d64992233bd421ac93523d6
Chribtoper <xcstolfax@gmail.com>
2019-01-07 23:07:49 -0500

    wss

--- src/components/Room.js
+++ src/components/Room.js
@@ -79,7 +79,7 @@ class Room extends Component {
 
   socketConnect = (currentRoomId) => {
     return new Promise ((resolve, reject) => {
-      this.cable = ActionCable.createConsumer("ws://viewingparty-app.herokuapp.com/cable");
+      this.cable = ActionCable.createConsumer("wss://viewingparty-app.herokuapp.com/cable");
       const roomSubscription = this.cable.subscriptions.create(
         {
           channel: "RoomsChannel",
