f205fc3d5778d176cf3dc7572526c335b6d63c66
Chribtoper <xcstolfax@gmail.com>
2019-01-10 00:18:08 -0500

    changed UI VH

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -88,7 +88,7 @@ class RoomsList extends Component {
       </Sidebar>
 
         <Sidebar.Pusher>
-          <Segment style={{height: '90vh'}} basic>
+          <Segment style={{height: '80vh'}} basic>
             { this.state.redirected ?
               <Redirect to={`/rooms/${this.state.roomId}`} render={window.location.reload()} />
                 :
